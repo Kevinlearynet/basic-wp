fe96806c5e0901647d7881c2dc688220d8eabd2e
Kevinleary.net <Info@kevinleary.net>
2025-01-18 20:22:37 -0500

    font adjustment

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <title><?php wp_title(''); ?></title>
   <meta charset="<?php  ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@100..900&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 <body <?php echo body_class(); ?>>
